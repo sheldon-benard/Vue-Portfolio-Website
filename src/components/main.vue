6e367ffd597d1fb19ff84f05973dfa45ebfb8c94
Sheldon Benard <imac@Sheldons-MacBook-Pro.local>
2018-05-05 23:32:10 -0400

    Fixes

--- src/components/main.vue
+++ src/components/main.vue
@@ -17,7 +17,7 @@
     <ul>
       <li>
         <a
-          href="../assets/pdf/Benard_Sheldon_Resume.pdf"
+          href="/static/pdf/Benard_Sheldon_Resume.pdf"
           target="_blank"
         >
           CV/Resume
@@ -25,7 +25,7 @@
       </li>
       <li>
         <a
-          href="../assets/pdf/Benard_Sheldon_General_Cover_Letter.pdf"
+          href="/static/pdf/Benard_Sheldon_General_Cover_Letter.pdf"
           target="_blank"
         >
           Cover Letter
